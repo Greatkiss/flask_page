7792e8d208dd3b4f216837da60b0c95d2b1770d9
Greatkiss <keitakanekomsazb@keio.jp>
2022-04-28 21:35:50 +0900

    for more application

--- Scraping_gnavi/scrape.py
+++ Scraping_gnavi/scrape.py
@@ -1,77 +1,84 @@
 #owner page
 #https://owner.tabelog.com/
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import openpyxl as xl
+import shutil
+import os
 import time
-
+e_list = []
 class get_table():
     @staticmethod
     def golden(name, id, pwd, date):
-        # セッションを開始
         session = requests.session()
-        url_login = "https://ssl.tabelog.com/owner_account/login/"
-        response = session.get(url_login)
-        res = session.get(url_login)
-        soup = BeautifulSoup(res.text,"html.parser")
-        auth_token = soup.find('input', {'name': 'authenticity_token'}).get('value')
-        response_cookie = response.cookies
-        # ログイン
-        login_info = {
-            "utf8":"✓",
-            "authenticity_token":auth_token,
-            "login_id":id,  #"login_id":id
-            "password":pwd, #"password":pwd
-            "login":"1",
-            "Target":""
-        }
-        # action
-        res = session.post(url_login, data=login_info, cookies=response_cookie)
-        res.raise_for_status() # エラーならここで例外を発生させる
-        soup_1 = BeautifulSoup(res.text,"html.parser")
-        mp_link = soup_1.select('#wrapper > div.op-rst-navi.op-rst-navi--owner > ul > li:nth-child(1)')[0].select('a')[0].get('href')
-        res = session.get(mp_link)
-        res.raise_for_status()
-        soup_2 = BeautifulSoup(res.text,"html.parser")
-        golden_time_link = soup_2.select('#wrapper > div.ol-container > div > div > div.ol-column2__side > div > div.op-side-nav > ul > li:nth-child(11)')[0].select('a')[0].get('href')
-        #golden_time
-        res = session.get(golden_time_link)
-        res.raise_for_status()
-        #tableの取得
-        raw_table=pd.DataFrame()
-        table = pd.DataFrame()
-        raw_table = pd.read_html(res.text)
-        #golden_timeのtable取得
-        table = raw_table[2]
-        table.rename(columns={'曜日（成約数）  ゴールデンタイム':'date'}, inplace=True)
-        table.rename(columns={'曜日（成約数）  ゴールデンタイム.1':'time'}, inplace=True)
-        table.rename(columns={'コール':'call'}, inplace=True)
-        table.rename(columns={'ネット予約':'online'}, inplace=True)
-        table.set_index(['date'], inplace=True)
+        try:
+            # セッションを開始
+            url_login = "https://ssl.tabelog.com/owner_account/login/"
+            response = session.get(url_login)
+            res = session.get(url_login)
+            soup = BeautifulSoup(res.text,"html.parser")
+            auth_token = soup.find('input', {'name': 'authenticity_token'}).get('value')
+            response_cookie = response.cookies
+            # ログイン
+            login_info = {
+                "utf8":"✓",
+                "authenticity_token":auth_token,
+                "login_id":id,  #"login_id":id
+                "password":pwd, #"password":pwd
+                "login":"1",
+                "Target":""
+            }
+            # action
+            res = session.post(url_login, data=login_info, cookies=response_cookie)
+            res.raise_for_status() # エラーならここで例外を発生させる
+            soup_1 = BeautifulSoup(res.text,"html.parser")
+            mp_link = soup_1.select('#wrapper > div.op-rst-navi.op-rst-navi--owner > ul > li:nth-child(1)')[0].select('a')[0].get('href')
+            res = session.get(mp_link)
+            res.raise_for_status()
+            soup_2 = BeautifulSoup(res.text,"html.parser")
+            golden_time_link = soup_2.select('#wrapper > div.ol-container > div > div > div.ol-column2__side > div > div.op-side-nav > ul > li:nth-child(11)')[0].select('a')[0].get('href')
+            #golden_time
+            res = session.get(golden_time_link)
+            res.raise_for_status()
+            #tableの取得
+            raw_table=pd.DataFrame()
+            table = pd.DataFrame()
+            raw_table = pd.read_html(res.text)
+            #golden_timeのtable取得
+            table = raw_table[2]
+            table.rename(columns={'曜日（成約数）  ゴールデンタイム':'date'}, inplace=True)
+            table.rename(columns={'曜日（成約数）  ゴールデンタイム.1':'time'}, inplace=True)
+            table.rename(columns={'コール':'call'}, inplace=True)
+            table.rename(columns={'ネット予約':'online'}, inplace=True)
+            table.set_index(['date'], inplace=True)
 
-        #selected-timeを取得
-        soup_table = BeautifulSoup(res.text,"html.parser")
-        selected_time_raw = soup_table.select('#wrapper > div.ol-container > div > div > div.ol-column2__main > div > div.weekly-intro-effect > table > tbody')[0].select('tr')
-        selected_time_list = []
-        for html_each_time in selected_time_raw:
-            th_name = html_each_time.find('td', class_='weekly-intro-effect__cell is-selected')
-            if th_name is None:
-                selected_time_list.append(0)
-            else:
-                selected_time_list.append(1)
-        table['selection']= selected_time_list
+            #selected-timeを取得
+            soup_table = BeautifulSoup(res.text,"html.parser")
+            selected_time_raw = soup_table.select('#wrapper > div.ol-container > div > div > div.ol-column2__main > div > div.weekly-intro-effect > table > tbody')[0].select('tr')
+            selected_time_list = []
+            for html_each_time in selected_time_raw:
+                th_name = html_each_time.find('td', class_='weekly-intro-effect__cell is-selected')
+                if th_name is None:
+                    selected_time_list.append(0)
+                else:
+                    selected_time_list.append(1)
+            table['selection']= selected_time_list
 
-        table.to_csv("./Scraping_gnavi/results/{}_{}.csv".format(name, date), encoding='shift-jis')
-        time.sleep(1)
-        #logout
-        logout_link = soup_table.select('#owner-headline > ul > li:nth-child(3) > a')[0].get('href')
-        res = session.get(logout_link)
-        soup_out = BeautifulSoup(res.text,"html.parser")
-        relogin_link = soup_out.select('#js-body > div.ol-container.ol-container--mat > div.ol-contents.ol-contents--fixed > div > p:nth-child(2) > a')[0].get('href')
-        session.get(relogin_link)
-        session.close()
+            table.to_csv("./Scraping_gnavi/results/{}_{}.csv".format(name, date), encoding='shift-jis')
+            time.sleep(1)
+            #logout
+            logout_link = soup_table.select('#owner-headline > ul > li:nth-child(3) > a')[0].get('href')
+            res = session.get(logout_link)
+            soup_out = BeautifulSoup(res.text,"html.parser")
+            relogin_link = soup_out.select('#js-body > div.ol-container.ol-container--mat > div.ol-contents.ol-contents--fixed > div > p:nth-child(2) > a')[0].get('href')
+            session.get(relogin_link)
+            session.close()
+            return []
+        except:
+            print("error at {}".format(name))
+            e_list.append([name, id, pwd])
+            session.close()
 
     def get_idpwd(n, path):
         niplist = pd.DataFrame()
@@ -83,6 +90,7 @@ class get_table():
             pwd = str(sheet.cell(row=i+1,column=3).value)
             nip = pd.DataFrame([[id, pwd]],columns=['id', 'pwd'], index=[name])
             niplist = pd.concat([niplist, nip])
+        shutil.rmtree("uploads")
         return niplist
 
 def main(num, path):
@@ -92,6 +100,8 @@ def main(num, path):
     for i in range(len(niplist)):
         print("log in to {}".format(niplist.index[i]))
         get_table.golden(niplist.index[i],niplist.iat[i,0], niplist.iat[i,1], "4_4-11")
+    pderror=pd.DataFrame(e_list)
+    pderror.to_csv("./error/errorlist.csv")
 
 if __name__ == "__main__":
     main(1,"./uploads/idpass.xlsx")
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 from flask import Flask, request, redirect, url_for, flash, render_template
 from werkzeug.utils import secure_filename
-from flask import send_from_directory
+import shutil
+import zipfile
 from Scraping_gnavi import scrape
 
-# 画像のアップロード先のディレクトリ
+# アップロード先のディレクトリ
 UPLOAD_FOLDER = './uploads'
+TEMP_FOLDER = './temp'
 # アップロードされる拡張子の制限
-ALLOWED_EXTENSIONS = set(['xlsx', 'csv', 'pdf'])
+ALLOWED_EXTENSIONS = set(['xlsx', 'csv', 'pdf','zip'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -17,7 +19,16 @@ def allwed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
-def uploads_file():
+def home():
+    work_list = ["scraping", "test", "test1"]
+    if request.method == 'POST':
+        action = request.form["what_to_do"]
+        if action == "scraping":
+            return redirect(url_for('uploads_files'))
+    return render_template("homepage.html", title="home", work_list = work_list)
+
+@app.route('/scrape', methods=['GET', 'POST'])
+def uploads_files():
     # リクエストがポストかどうかの判別
     if request.method == 'POST':
         # ファイルがなかった場合の処理
@@ -35,12 +46,22 @@ def uploads_file():
             # 危険な文字を削除（サニタイズ処理）
             filename = secure_filename(file.filename)
             # ファイルの保存
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            os.mkdir(TEMP_FOLDER)
+            os.mkdir(UPLOAD_FOLDER)
+            file.save(os.path.join(TEMP_FOLDER, filename))
+            # ファイルの解凍
+            with zipfile.ZipFile(TEMP_FOLDER+'/'+filename, "r") as zp:
+                try:
+                    zp.extractall(path = UPLOAD_FOLDER, pwd="kanekokeita".encode("utf-8"))
+                    print("extract is complite")
+                except RuntimeError as e:
+                    print(e)
+            shutil.rmtree(TEMP_FOLDER)
             # アップロード後のページに転送
             return redirect(url_for('confirm_info'))
     return render_template("uploadpage.html", title="upload", name="Hoge")
 
-@app.route('/confirm_info', methods=['GET','POST'])
+@app.route('/scrape/confirm_info', methods=['GET','POST'])
 def confirm_info():
     path = './uploads'
     files = []
@@ -57,7 +78,7 @@ def confirm_info():
         return redirect(url_for('run_da_scrape',num = n, excelname = ipfilename))
     return render_template("confirm.html", title="confirm", idpasslist = idpasslist)
 
-@app.route('/run/<excelname>/<num>')
+@app.route('/scrape/run/<excelname>/<num>')
 def run_da_scrape(num, excelname):
     print("RUN DA SCRAPE with the {}".format(excelname))
     scrape.main(int(num), "./uploads/{}".format(excelname))
